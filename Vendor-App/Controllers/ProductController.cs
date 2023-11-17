6b20cee923695e8287c7b471b518e9fab49e1bfe
Mustafa Shakir <mustaphashakir2@gmail.com>
2023-11-17 13:01:12 +0000

    Added product folder

--- Vendor-App/Controllers/ProductController.cs
+++ Vendor-App/Controllers/ProductController.cs
@@ -1,12 +1,190 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vendor_App.Models;
+using Vendor_App.Views;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Vendor_App.Controllers
 {
     internal class ProductController
     {
+        Models.Product product = new Models.Product();
+
+        public static void CreateProduct(int CompanyID, string SoftwareName, string TypeOfSoftware, string Description)
+        {
+            
+            try
+            {
+              
+                SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\musta\\Source\\Repos\\vendor-application\\Vendor-App\\Vendor.mdf;Integrated Security=True;Connect Timeout=30");
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Insert into [Product] values (@CompanyID, @SoftwareName, @TypeOfSoftware, @Description)", conn);
+
+                cmd.Parameters.AddWithValue("CompanyID", CompanyID);
+                cmd.Parameters.AddWithValue("SoftwareName", SoftwareName);
+                cmd.Parameters.AddWithValue("TypeOfSoftware", TypeOfSoftware);
+                cmd.Parameters.AddWithValue("Description", Description);
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                MessageBox.Show("Product Added.");
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        public static void UpdateProduct(int ProductID, int CompanyID, string SoftwareName, string TypeOfSoftware, string Description)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\musta\\Source\\Repos\\vendor-application\\Vendor-App\\Vendor.mdf;Integrated Security=True;Connect Timeout=30");
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Update [Product] set CompanyID=@CompanyID, SoftwareName=@SoftwareName, TypeOfSoftware=@TypeOfSoftware, Description=@Description where ProductID=@ProductID", conn);
+
+                cmd.Parameters.AddWithValue("ProductID", ProductID);
+                cmd.Parameters.AddWithValue("CompanyID", CompanyID);
+                cmd.Parameters.AddWithValue("SoftwareName", SoftwareName);
+                cmd.Parameters.AddWithValue("TypeOfSoftware", TypeOfSoftware);
+                cmd.Parameters.AddWithValue("Description", Description);
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                MessageBox.Show("Product Information Updated.");
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static void DeleteProduct(int ProductID)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\musta\\Source\\Repos\\vendor-application\\Vendor-App\\Vendor.mdf;Integrated Security=True;Connect Timeout=30");
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Delete Product WHERE ProductID=@ProductID", conn);
+
+                cmd.Parameters.AddWithValue("@ProductID", ProductID);
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                MessageBox.Show("Product Deleted.");
+            }
+
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public static void SearchByID(int ProductID)
+        {
+            
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\musta\\Source\\Repos\\vendor-application\\Vendor-App\\Vendor.mdf;Integrated Security=True;Connect Timeout=30");
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Product WHERE ProductID=@ProductID", conn);
+
+                cmd.Parameters.AddWithValue("@ProductID", ProductID);
+
+                cmd.ExecuteNonQuery();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if(reader.HasRows)
+                {
+                    reader.Read();
+                    string ProductIdentification = reader["ProductID"].ToString();
+                    string CompanyIdentification = reader["CompanyID"].ToString();
+                    string NameOfSoftware = reader["SoftwareName"].ToString() ;
+                    string SoftwareType = reader["TypeOfSoftware"].ToString();
+                    string SoftwareDescription = reader["Description"].ToString();
+
+                    MessageBox.Show($"Product ID: {ProductIdentification}\n\nCompany ID: {CompanyIdentification}\n\nSoftware Name: {NameOfSoftware}\n\nType of Software: {SoftwareType}\n\nDescription: {SoftwareDescription}");
+
+                }
+
+                else
+                {
+                    MessageBox.Show("Product not found.");
+                }
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show("Please check if you have entered the correct product ID");
+            }
+        }
+
+        public static void SearchByName(string SoftwareName)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\musta\\Source\\Repos\\vendor-application\\Vendor-App\\Vendor.mdf;Integrated Security=True;Connect Timeout=30");
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Product WHERE SoftwareName=@SoftwareName", conn);
+
+                cmd.Parameters.AddWithValue("@SoftwareName", SoftwareName);
+
+                cmd.ExecuteNonQuery();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    string ProductIdentification = reader["ProductID"].ToString();
+                    string CompanyIdentification = reader["CompanyID"].ToString();
+                    string NameOfSoftware = reader["SoftwareName"].ToString();
+                    string SoftwareType = reader["TypeOfSoftware"].ToString();
+                    string SoftwareDescription = reader["Description"].ToString();
+
+                    MessageBox.Show($"Product ID: {ProductIdentification}\n\nCompany ID: {CompanyIdentification}\n\nSoftware Name: {NameOfSoftware}\n\nType of Software: {SoftwareType}\n\nDescription: {SoftwareDescription}");
+
+                }
+
+                else
+                {
+                    MessageBox.Show("Product not found.");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please check if you have entered the correct software name");
+            }
+        }
+
+
+
     }
 }
--- Vendor-App/Views/Product.cs
+++ Vendor-App/Views/Product.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Vendor_App.Controllers;
+using Vendor_App.Models;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Vendor_App.Views
 {
@@ -17,12 +21,135 @@ namespace Vendor_App.Views
             InitializeComponent();
         }
 
+        // Go Back Button.
         private void button1_Click(object sender, EventArgs e)
         {
             Form1 form1 = new Form1();
             form1.Show();
 
             this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Add button.
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                ProductController.CreateProduct(int.Parse(textBox2.Text), textBox3.Text, textBox4.Text, textBox5.Text);
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show("Sorry, product could not be added.");
+            }
+        }
+
+        //Update Button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                ProductController.UpdateProduct(int.Parse(textBox1.Text),int.Parse(textBox2.Text), textBox3.Text, textBox4.Text, textBox5.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Sorry, the product could not be updated. " +
+                    "Maybe the details you entered did not match with database in order to update it.");
+            }
+        }
+
+        // Delete Button.
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ProductController.DeleteProduct(int.Parse(textBox1.Text));
+            }
+            
+            catch(Exception ex)
+            {
+                MessageBox.Show("Sorry, the product could not be deleted. Please check if the Product ID is correct.");
+            }
+        }
+
+        // Search by ID button.
+        private void button5_Click(object sender, EventArgs e)
+        {
+           ProductController.SearchByID(int.Parse(textBox1.Text));
+
+        }
+
+        // Search by Name button.
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ProductController.SearchByName(textBox3.Text);
+        }
+
+        // View All Button
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\musta\\Source\\Repos\\vendor-application\\Vendor-App\\Vendor.mdf;Integrated Security=True;Connect Timeout=30");
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from Product", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
         }
     }
 }
